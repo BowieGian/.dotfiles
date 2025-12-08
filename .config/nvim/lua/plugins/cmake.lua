fa7792568b6c9625a881a1539c189fc7195bf0b1
BowieGian <43588714+BowieGian@users.noreply.github.com>
2025-12-08 02:45:11 -0800

    Update CMake opener hotkey to work during vertical splits

--- .config/nvim/lua/plugins/cmake.lua
+++ .config/nvim/lua/plugins/cmake.lua
@@ -6,7 +6,7 @@ return {
 		vim.g.cmake_link_compile_commands = 1
 		vim.g.cmake_console_size = 36
 
-		vim.keymap.set("n", "<leader>cq", ":CMakeToggle<CR><C-w><C-w>")
+		vim.keymap.set("n", "<leader>cq", ":CMakeToggle<CR><C-w>j")
 
 		-- <C-z> is a wildcharm to invoke completion from the command line
 		vim.keymap.set("n", "<leader>cr", ":CMakeRun <C-z><CR>")
